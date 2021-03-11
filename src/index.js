b9b226de70a6591565183e4754b279ffe2ce3a3d
mroforolhc <mroforolhc@gmail.com>
2021-03-11 22:48:21 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  let isCorrect = true;
+
+  str.split('').forEach((bracket) => {
+    if (isCorrect) {
+      bracketsConfig.forEach((array) => {
+        // Если скобка открытая – в стек, если закрытая – достаём открытую из стека и проверяем
+        if (bracket === array[0]) {
+            if (array[0] === array[1] && bracket === stack[stack.length - 1]) stack.pop();
+            else stack.push(bracket);
+        }
+        else if ((bracket === array[1]) && (stack.pop() !== array[0])) isCorrect = false;
+      })
+    }
+  });
+
+  if (isCorrect && !stack.length) return true;
+  return false;
 }
